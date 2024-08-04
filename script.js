e5b6d5bdfe6aebf22a655067439017b379e5f027
João Santos <32807182+jvpdls@users.noreply.github.com>
2024-08-04 20:53:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function isMobile() {
-  // Usa uma expressão regular para verificar o agente do usuário (user agent)
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
@@ -7,14 +6,12 @@ function insertBanner() {
   const bannerContainer = document.getElementById("top-banner");
 
   if (isMobile()) {
-  // Código para dispositivos móveis
   bannerContainer.innerHTML = `
     <a id="top-ad" href="" target="_blank" rel="sponsored">
       <img src="" width="" height="" alt=""/>
     </a>
   `;
   } else {
-  // Código para desktop e tablets
   bannerContainer.innerHTML = `
     <a id="top-ad" href="" target="_blank" rel="sponsored">
       <img src="" width="" height="" alt=""/>
@@ -23,5 +20,4 @@ function insertBanner() {
   }
 }
 
-// Chama a função para inserir o banner com base no dispositivo
 insertBanner();
